61b45f9c486f2187fb002246a396f4fba9abd33e
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-29 06:42:45 +0200

    commit

--- app/src/main/java/com/example/user/myprogrammenu/MainActivity.java
+++ app/src/main/java/com/example/user/myprogrammenu/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.user.myprogrammenu;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -18,18 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // добавляем пункты меню
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
 /*
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.game_menu, menu);
+            menu.add(Menu.NONE, IDM_OPEN, Menu.NONE, "Открыть");
+            menu.add(Menu.NONE, IDM_SAVE, Menu.NONE, "Сохранить");
 */
 
-        menu.add(Menu.NONE, IDM_OPEN, Menu.NONE, "Открыть");
-        menu.add(Menu.NONE, IDM_SAVE, Menu.NONE, "Сохранить");
-        return true;
-    }
+            getMenuInflater().inflate(R.menu.menu, menu);
+
+/*
+            menu.add(Menu.NONE, IDM_OPEN, Menu.NONE, "Открыть").setIcon(android.R.drawable.ic_menu_help);
+            menu.add(Menu.NONE, IDM_SAVE, Menu.NONE, "Сохранить").setIcon(R.drawable.ic_launcher);;
+*/
+
+            return true;
+            //return(super.onCreateOptionsMenu(menu));
+        }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -43,6 +52,13 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case IDM_SAVE:
                 infoTextView.setText("Вы выбрали пункт СОХРАНИТЬ");
+            case R.id.menu_open:
+                infoTextView.setText("Вы выбрали пункт ОТКРЫТЬ");
+                return true;
+            case R.id.menu_save:
+                infoTextView.setText("Вы выбрали пункт СОХРАНИТЬ");
+                return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
